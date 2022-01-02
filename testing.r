5450918e841950e98c5516247f07b5095d569cc0
jabreenjab <jabreenahmed>
2022-01-02 10:45:02 +0500

    added few lines.

--- testing.r
+++ testing.r
@@ -1,2 +1,8 @@
 # Hello_r
 print("hello welcome ti github")
+
+name="jabreen"
+what="it is just sample lines added for practice."
+print(name)
+print(what)
+print ("I am back with other changes as well")
